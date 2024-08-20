22096a5805c2b0f804ccaed07c851c99947a3891
Hafizh <152354769+FHHIZ@users.noreply.github.com>
2024-08-20 22:08:09 +0700

    Add files via upload

---
+++ tugas7/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').textContent = 'Latihan JavaScript Selesai';
+});
+
+let name = "John";
+const age = "30";
+
+const output = document.getElementById('output').innerHTML = `
+    <p>Name: ${name}</p>
+    <p>Age: ${age}</p>
+`;
+
+let fruit = ["Apple", "Banana", "Manggo"]; 
+fot
