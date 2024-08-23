21bf71d851530f34e9eca3151b9c5fbaed206403
Hafizh <152354769+FHHIZ@users.noreply.github.com>
2024-08-23 00:16:53 -0800

    script.js

--- tugas7/script.js
+++ tugas7/script.js
@@ -5,13 +5,51 @@ document.addEventListener('DOMContentLoaded', function() {
 let name = "John";
 const age = "30";
 
-const output = document.getElementById('output').innerHTML = `
+document.getElementById('output0').innerHTML = `
     <p>Name: ${name}</p>
     <p>Age: ${age}</p>
 `;
 
-let fruit = ["Apple", "Banana", "Manggo"]; 
-let fruitbasket = document.getElementById("output")
-for (let i = 0; i < fruit.length; i++) {
-    let threefruit = document.createElement("p")
+function greet(name) {
+    return "Hello, " + name;
 }
+
+document.getElementById('output1').innerHTML = greet(name);
+
+let fruit = ["Apple", "Banana", "Mango"];
+let fruitbasket = document.getElementById("output2");
+for (let i = 0; i < fruit.length; i++) {
+    let threefruit = document.createElement("p");
+    threefruit.textContent = fruit[i];
+    fruitbasket.appendChild(threefruit);
+}
+
+function cekNilai() {
+    var number = parseInt(document.getElementById('userinput').value);
+    var hasil;
+    if (number > 10) {
+        hasil = "Lebih besar dari 10";
+    } else if (number === 10) { 
+        hasil = "Sama dengan 10";
+    } else {
+        hasil = "Lebih kecil dari 10";
+    }
+    document.getElementById('hasil').innerHTML = hasil;
+}
+
+function calculateSquare(x, y) {
+    var z = x * y
+    return z
+}
+document.getElementById("output3").innerHTML = calculateSquare(2, 2);
+let arrowFunction = (a, b) => a * b;
+document.getElementById("output4").innerHTML = arrowFunction(4, 4);
+
+let students = ["Alice", "Bob", "Charlie"]; 
+students.push("Kevin");
+let output = "";
+
+students.forEach((item, index) => {
+    output += index + "; " + item + "<br>";
+});
+document.getElementById("output5").innerHTML = output;
